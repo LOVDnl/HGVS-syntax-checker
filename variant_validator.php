9bd4461a09be66825c7d60c6b40488f47e3763aa
Ivo Fokkema <i.f.a.c.fokkema@lumc.nl>
2025-08-12 20:18:26 +0200

    Add a repeater with wait for getTranscriptsByID().
    It fails quite a bit, so make sure we retry a few times, with a
     longer wait each time.

--- variant_validator.php
+++ variant_validator.php
@@ -452,7 +452,7 @@ class LOVD_VV
 
 
 
-    public function getTranscriptsByID ($sSymbol)
+    public function getTranscriptsByID ($sSymbol, $aOptions = [])
     {
         // Returns the available transcripts for the given gene or transcript.
         // When a transcript has been passed, it returns only that transcript (any version).
@@ -525,7 +525,22 @@ class LOVD_VV
                     ]
                 );
             }
-            return array_merge($this->aResponse, ['errors' => 'No transcripts found.']);
+
+            // We get this quite a bit in LOVD+. Perhaps we need a longer wait.
+            if (empty($aOptions['repeated_call'])) {
+                $aOptions['repeated_call'] = 0;
+            }
+
+            // Repeat a few times, if needed.
+            if ($aOptions['repeated_call'] < 4) {
+                // This adds some overhead (re-processing of input and a recursive function call),
+                //  but it's the simplest method.
+                $aOptions['repeated_call'] ++;
+                usleep($this->nMicroSecondsToSleep * ($aOptions['repeated_call'] + 1));
+                return $this->getTranscriptsByID($sSymbol, $aOptions);
+            } else {
+                return array_merge($this->aResponse, ['errors' => ['No transcripts found.']]);
+            }
         }
 
         $aData = $this->aResponse;
